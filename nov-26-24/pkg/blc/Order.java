2d58ef698fb5719909a3661d0042a0bfed119032
Kalyani-Nanna <mothkurkalyani3@gmail.com>
2025-01-04 23:43:59 +0530

    Update Order.java

--- nov-26-24/pkg/blc/Order.java
+++ nov-26-24/pkg/blc/Order.java
@@ -1,1 +1,32 @@
 
+//BLC class of Shopping
+
+package pkg.blc;
+
+public class Order
+{
+    private String orderId;
+    private String productName;
+
+    public Order(String orderId, String productName)
+    {
+        this.orderId = orderId;
+        this.productName = productName;
+    }
+
+    public String getOrderId()
+    {
+        return orderId;
+    }
+
+    public String getProductName()
+    {
+        return productName;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "Order Id : "+orderId+", Product Name : "+productName;
+    }
+}
