ef82dcbdfc1bd324b4259cb70a5486fdd407ae11
Kalyani-Nanna <mothkurkalyani3@gmail.com>
2025-01-04 23:44:23 +0530

    Update Engine.java

--- nov-26-24/pkg/blc/Engine.java
+++ nov-26-24/pkg/blc/Engine.java
@@ -1,2 +1,26 @@
 
-Engine
+//BLC class of TestCar
+
+package com.blc;
+
+public class Engine
+{
+    private String model;
+
+    public Engine(String model)
+    {
+        this.model = model;
+    }
+
+    public String getModel()
+    {
+        return model;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "Engine Model = "+model;
+    }
+
+}
