3a3d4ec32c8269964b6dde16c08e398066973163
Kalyani-Nanna <mothkurkalyani3@gmail.com>
2025-01-04 23:05:16 +0530

    Update InvalidAmountException.java

--- jan_3_2025/pkg/InvalidAmountException.java
+++ jan_3_2025/pkg/InvalidAmountException.java
@@ -1,1 +1,11 @@
 
+// Userdefined RuntimeException of ATM Operations -> InvalidAmountException.java
+
+package pkg;
+
+public class InvalidAmountException extends RuntimeException 
+{
+	public InvalidAmountException(String errorMessage) {
+		super(errorMessage);
+	}
+}
