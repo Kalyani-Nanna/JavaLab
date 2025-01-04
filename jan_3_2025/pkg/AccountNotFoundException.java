78f1dda8f6bee0315df57f2bf73c832fd64abde6
Kalyani-Nanna <mothkurkalyani3@gmail.com>
2025-01-04 23:02:06 +0530

    Update AccountNotFoundException.java

--- jan_3_2025/pkg/AccountNotFoundException.java
+++ jan_3_2025/pkg/AccountNotFoundException.java
@@ -1,1 +1,11 @@
 
+// Userdefined Exception of ATM Operations -> AccountNotFoundException.java
+
+package pkg;
+
+public class AccountNotFoundException extends Exception 
+{
+	public AccountNotFoundException(String errorMessage) {
+		super(errorMessage);
+	}
+}
