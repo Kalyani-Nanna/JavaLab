c999099208a0c00a361ce89c620164d00a51b967
Kalyani-Nanna <mothkurkalyani3@gmail.com>
2025-01-05 19:57:26 +0530

    Update CircleArea.java

--- Packages/CircleArea.java
+++ Packages/CircleArea.java
@@ -1,1 +1,23 @@
 
+package com.dev.tools;
+
+import com.dev.tools.Circle;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class CircleArea
+{
+	public static void main(String[] args) 
+	{   
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the radius :");
+		double radius = sc.nextDouble();
+		
+		String areaOfCircle = Circle.getAreaOfCircle(radius);	
+		System.out.println(areaOfCircle);
+		
+		sc.close();
+	
+	}
+}
