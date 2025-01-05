3927e0b9c50efb1b3302b028a0588cd90794b19d
Kalyani-Nanna <mothkurkalyani3@gmail.com>
2025-01-05 20:09:46 +0530

    Update Circle.java

--- Packages/com/dev/tools/Circle.java
+++ Packages/com/dev/tools/Circle.java
@@ -1,1 +1,19 @@
-Circle
+package com.dev.tools;
+
+public class Circle 
+{
+  public static String getAreaOfCircle(double radius)
+  {		 
+	  if(radius <=0)
+	  {
+		  return ""+(-1);
+	  }
+	  else
+	  {
+		  final double PI = 3.14;
+		  double areaOfCircle = PI * radius * radius;
+		  return ""+areaOfCircle;
+	  }
+	  
+  }
+}
