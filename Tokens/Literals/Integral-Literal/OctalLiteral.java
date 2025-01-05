b3a5f8fb343c03066670fbab20f3bd1a2e4caeeb
Kalyani-Nanna <mothkurkalyani3@gmail.com>
2025-01-05 19:51:17 +0530

    Update OctalLiteral.java

--- Tokens/Literals/Integral-Literal/OctalLiteral.java
+++ Tokens/Literals/Integral-Literal/OctalLiteral.java
@@ -1,1 +1,23 @@
 
+/*
+If any Integer literal starts with 0 (Zero) then it will become octal literal. 
+Here base is 8 so it will accept 8 digits i.e 0 to 7.
+*/
+
+package com.classes;
+
+public class OctalLiteral
+{
+	public static void main(String args[])
+	{
+		int a = 0123;		//Octal literal
+		int b = 0;			//Octal literal
+		int c = 0777;		//Octal literal
+		int d = 000;		//Octal literal
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+	}
+}
