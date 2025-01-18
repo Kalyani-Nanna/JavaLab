a6f1baa40ad1e3f3ae77ab4870bf2e1cb15ee0fa
mothkur kalyani <mothkurkalyani3@gmail.com>
2025-01-18 21:14:14 +0530

    Update CarPunctureException.java

--- Jan-Tasks/pkg/CarPunctureException.java
+++ Jan-Tasks/pkg/CarPunctureException.java
@@ -1,1 +1,12 @@
 
+////BLC class of CarUser -> CarPunctureException.java
+
+package pkg;
+
+public class CarPunctureException extends Exception
+{
+	public CarPunctureException(String puncture)
+	{
+		super(puncture);
+	}
+}
