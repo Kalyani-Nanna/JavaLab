b9d4b90a2fd935ac50578e55ea76288d0ae0af67
mothkur kalyani <mothkurkalyani3@gmail.com>
2025-01-18 21:14:54 +0530

    Update CarStoppedException.java

--- Jan-Tasks/pkg/CarStoppedException.java
+++ Jan-Tasks/pkg/CarStoppedException.java
@@ -1,1 +1,11 @@
 
+//BLC class of CarUser -> CarStoppedException.java
+
+package pkg;
+
+public class CarStoppedException extends Exception
+{	
+	public CarStoppedException(String stop) {
+		super(stop);
+	}
+}
