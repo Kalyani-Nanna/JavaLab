f70d7d4d3ff1168eee740c209dd32e008a69b9e7
mothkur kalyani <mothkurkalyani3@gmail.com>
2025-01-18 21:25:44 +0530

    Update LoanNotAllowedException.java

--- Jan-Tasks/3-Jan-25/pkg/LoanNotAllowedException.java
+++ Jan-Tasks/3-Jan-25/pkg/LoanNotAllowedException.java
@@ -1,1 +1,10 @@
+// Userdefined RuntimeException of ATM Operations -> LoanNotAllowedException.java
 
+package pkg;
+
+public class LoanNotAllowedException extends RuntimeException
+{
+	public LoanNotAllowedException(String errorMessage) {
+		super(errorMessage);
+	}
+}
