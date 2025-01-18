1cfc3cf6f003ec0f0f09997b42460cd0f6fa12f3
mothkur kalyani <mothkurkalyani3@gmail.com>
2025-01-18 21:24:38 +0530

    Update InsufficientFundsException.java

--- Jan-Tasks/3-Jan-25/pkg/InsufficientFundsException.java
+++ Jan-Tasks/3-Jan-25/pkg/InsufficientFundsException.java
@@ -1,1 +1,11 @@
 
+// Userdefined Exception of ATM Operations -> InsufficientFundsException.java
+
+package pkg;
+
+public class InsufficientFundsException extends Exception 
+{
+	public InsufficientFundsException(String errorMessage) {
+		super(errorMessage);
+	}
+}
